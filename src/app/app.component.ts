9dde18f3f75f796add1d81885b8a3b63925aeeed
Felipe Berrutti <feliberrutti@outlook.com>
2025-03-01 04:22:02 -0300

    add menu mobile transition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output } from '@angular/core';
+import { Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
 import { PresentationComponent } from './presentation/presentation.component';
@@ -6,8 +6,8 @@ import { TechnologiesComponent } from './technologies/technologies.component';
 import { ProyectsComponent } from './proyects/proyects.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
-import { CommonModule, NgIf } from '@angular/common';
 import { CertificationsComponent } from './certifications/certifications.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,20 +1,69 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'menu',
   standalone: true,
   imports: [],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.css'
+  styleUrl: './menu.component.css',
+  animations: [
+    trigger('menuAn', [
+      state('initial', style({
+        maxWidth: '0px'
+      })),
+      state('final',style({
+        maxWidth: '100px',
+        minWidth: '50%'
+      })),
+      transition('initial => final',
+      animate('500ms')),
+      transition('final => initial',
+      animate('500ms'))
+    ]),
+    trigger('menuOptionsAn', [
+      state('initial', style({
+        opacity: 0
+      })),
+      state('final',style({
+        opacity: 1
+      })),
+      transition('initial => final',
+      animate('500ms')),
+      transition('final => initial',
+      animate('150ms'))
+    ])
+  ]
 })
 export class MenuComponent {
   isMenuVisible: boolean = false;
+  auxAnimation = 'final';
+  auxOptionsAnimation = 'final';
+
+  openMenu(): void{
+      this.auxAnimation = 'initial';
+      setTimeout(() => {
+        this.auxOptionsAnimation = 'initial';
+      }, 0);
+      this.isMenuVisible = true;
+
+  };
+
+  closeMenu(): void{
+      this.auxAnimation = 'final';
+      setTimeout(() => {
+        this.auxOptionsAnimation = 'final';
+      }, 500);
+      this.isMenuVisible = false;
+  };
+
+
 
   handleMenuClick(): void{
     this.isMenuVisible ?
-    this.isMenuVisible = false :
-    this.isMenuVisible = true
+    this.closeMenu() :
+    this.openMenu()
   };
 
   menuOptionsList: Array<string> = ['Tecnologías','Proyectos',
