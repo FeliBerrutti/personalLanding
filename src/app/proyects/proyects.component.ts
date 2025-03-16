36ae809ed561594d3c69d3d7753da7015145c795
Felipe Berrutti <feliberrutti@outlook.com>
2025-03-15 23:38:16 -0300

    add crm link to proyects

--- src/app/proyects/proyects.component.ts
+++ src/app/proyects/proyects.component.ts
@@ -24,8 +24,8 @@ export class ProyectsComponent {
   handleCardClick(aux: number): void{
     aux === 0 ?
     window.open('https://feliberrutti.github.io/EL-10-VP', '_blank') :
-    aux === 2 ?
-    window.open('https://github.com/FeliBerrutti/personalLanding', '_blank') :
-    console.log('');
+    aux === 1 ?
+    window.open('https://feliberrutti.github.io/velCRM/') :
+    window.open('https://github.com/FeliBerrutti/personalLanding', '_blank')
   };
 }
