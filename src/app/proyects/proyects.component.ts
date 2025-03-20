6e3c4a80c691847bec090d5a9403c30e472c03d0
Felipe Berrutti <LBdevs@outlook.com>
2025-03-20 14:54:47 -0300

    add restaurant to proyects
    .

--- src/app/proyects/proyects.component.ts
+++ src/app/proyects/proyects.component.ts
@@ -9,14 +9,15 @@ import { CommonModule } from '@angular/common';
   styleUrl: './proyects.component.css'
 })
 export class ProyectsComponent {
-  proyectsList: Array<string> = ['El 10 de Villa Pueyrredón','CRM - Versión Operador','Homepage'];
+  proyectsList: Array<string> = ['El 10 de Villa Pueyrredón','CRM - Versión Operador','Homepage','Restaurante Las Victorias'];
 
-  proyectsImgList: Array<string> = ['EL10VP.gif','CRM.gif','landingShow.gif'];
+  proyectsImgList: Array<string> = ['EL10VP.gif','CRM.gif','landingShow.gif','restaurantGIF.gif'];
 
   proyectTextList: Array<string> = [` Landing page para negocio de alquiler de volquetes.`,
                                     `Sistema de gestión de relaciones con clientes.
                                     Usuario y contraseña: prueba`,
-                                    `Mi pagina personal, link al repositorio de GitHub.`];
+                                    `Mi pagina personal, link al repositorio de GitHub.`,
+                                    `Pagina web de muestra para restaurantes.`];
 
   handleIMG(aux: number): string{
     return this.proyectsImgList[aux];
@@ -26,7 +27,9 @@ export class ProyectsComponent {
     aux === 0 ?
     window.open('https://feliberrutti.github.io/EL-10-VP', '_blank') :
     aux === 1 ?
-    window.open('https://feliberrutti.github.io/velCRM/') :
-    window.open('https://github.com/FeliBerrutti/personalLanding', '_blank')
+    window.open('https://feliberrutti.github.io/velCRM/', '_blank') :
+    aux === 2 ?
+    window.open('https://github.com/FeliBerrutti/personalLanding', '_blank') :
+    window.open('https://lbsoftwaredevs.github.io/muestraRestaurante/', '_blank')
   };
 }
